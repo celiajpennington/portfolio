7722678b24e21246e4719113fbddde97a1b00fc0
Celia Pennington <celiajpennington@gmail.com>
2021-08-25 16:22:04 -0500

    added image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../../style/Home.css';
-
+import Footer from './Footer';
 export default function Home() {
   return (
     <div className="main-div">
       <h1 className="home-name">Celia Pennington</h1>
-      
+      <Footer></Footer>
     </div>
   );
 }
