2833cb590abbd8b07176e415d7b71a883cf4f368
Celia Pennington <celiajpennington@gmail.com>
2021-08-24 21:34:42 -0500

    added website images and portfolio page styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import "../../style/Portfolio.css"
+
 
 export default function Portfolio() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <h1>My Work</h1>
+
+
+      <img  className ="sous-chef"src="https://i.imgur.com/HVGngPv.png" alt="recipe website"></img>
+
+
+
+
+        <img src="https://i.imgur.com/yLoMqef.png" alt="travel website"></img>
+
+
+          
+            <img src="https://i.imgur.com/kEoToXP.png" alt="pink code quiz"></img>
+
+
+
+              <img src="#" alt="solid colored sneakers with white soles against a white backdrop"></img>
+
+
     </div>
-  );
+              );
 }
