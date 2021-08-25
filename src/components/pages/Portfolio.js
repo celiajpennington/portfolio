54cac67f4df66e11c1d8a675676eb90ae69f3519
Celia Pennington <celiajpennington@gmail.com>
2021-08-25 16:00:15 -0500

    added github pages

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -9,19 +9,23 @@ export default function Portfolio() {
 
 
       <img  className ="sous-chef"src="https://i.imgur.com/HVGngPv.png" alt="recipe website"></img>
+      <h3>Sous-Chef</h3>
 
 
 
 
         <img src="https://i.imgur.com/yLoMqef.png" alt="travel website"></img>
+        <h3>Top Tourist</h3>
 
 
           
             <img src="https://i.imgur.com/kEoToXP.png" alt="pink code quiz"></img>
+            <h3>Code Quiz</h3>
 
 
 
               <img src="https://i.imgur.com/UDdW4Sb.png" alt="workout tracker app"></img>
+              <h3>Workout Tracker</h3>
 
 
     </div>
