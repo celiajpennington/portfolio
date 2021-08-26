b9ae6c2d7927d36f21b7246700f69bceb907b4ab
Celia Pennington <celiajpennington@gmail.com>
2021-08-26 14:15:41 -0500

    added media to home and portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,20 +7,20 @@ import { Nav, NavItem, NavLink } from 'reactstrap';
 
 const Portfolio = (props) => {
   return (
-    <div>
+    <div className="portfolio-div">
        <h1 className="portfolio-title">My Work</h1>
       <Nav vertical>
       <img className="sous-chef" src="https://i.imgur.com/HVGngPv.png" alt="recipe website"></img>
         <NavItem>
-          <NavLink style={{ textDecoration: "none", color: "#D99E89" }} href="https://sous-chef-project-2.herokuapp.com/">Sous-Chef</NavLink>
+          <NavLink style={{ textDecoration: "none", color: "#D99E89", fontSize: "50px" }} href="https://sous-chef-project-2.herokuapp.com/">Sous-Chef</NavLink>
         </NavItem>
         <NavItem>
         <img src="https://i.imgur.com/yLoMqef.png" alt="travel website"></img>
-          <NavLink style={{ textDecoration: "none", color: "#D99E89" }} href="https://jguiro09.github.io/TopTourist/">Top Tourist</NavLink>
+          <NavLink style={{ textDecoration: "none", color: "#D99E89", fontSize: "50px" }} href="https://jguiro09.github.io/TopTourist/">Top Tourist</NavLink>
         </NavItem>
         <NavItem>
         <img src="https://i.imgur.com/kEoToXP.png" alt="pink code quiz"></img>
-          <NavLink style={{ textDecoration: "none", color: "#D99E89" }} href="https://celiajpennington.github.io/code.Quiz/">Code Quiz</NavLink>
+          <NavLink style={{ textDecoration: "none", color: "#D99E89", fontSize: "50px" }} href="https://celiajpennington.github.io/code.Quiz/">Code Quiz</NavLink>
         </NavItem>
         
       </Nav>
