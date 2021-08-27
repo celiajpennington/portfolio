b53e199b739da9f2325de3ea1404b756f43ab644
Celia Pennington <celiajpennington@gmail.com>
2021-08-26 19:47:28 -0500

    added resume styling and link

--- README.md
+++ README.md
@@ -2,6 +2,70 @@
 
 https://celiajpennington.github.io/portfolio/
 
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Visuals](#visuals)
+4. [Usage](#usage)
+5. [License](#license)
+6. [Contribution](#contribution)
+7. [Questions](#questions)
+
+## Description
+Creating a portfolio with a contact form using React
+
+## Installation & Technology
+``` npm i ``` <br>
+``` npm start ``` <br>
+
+React<br>
+Reactstrap<br>
+JavaScript <br>
+HTML <br>
+CSS <br>
+
+## Visuals
+ ### Usage Video
+
+## License 
+This application is covered under The MIT License
+
+Copyright (c) 2011 Dominic Tarr
+
+Permission is hereby granted, free of charge, 
+to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to 
+deal in the Software without restriction, including 
+without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom 
+the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice 
+shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+## Contribution
+Robby Kurle <br>
+
+
+
+## Questions
+If you have any questions you can reach me at: <br>
+<a href = 'celiajpennington@gamil.com' > celiajpennington@gmail.com </a>
+
+
+
+
 
 ## Your Task
 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import '../../style/Resume.css';
 import Footer from './Footer';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 export default function Resume() {
   return (
-    <div>
-      <h2>Professional Summary</h2>
-      <p>
+    <div className="resume">
+      <h2 className="professional">Professional Summary</h2>
+      <p className="summary"> 
         Hard working, passionate and determined full stack web development student. I believe my retail management, customer service and human resources experience combined with my newly developed and continuously evolving web design skills give me a competitive edge in the web development industry. I enjoy a challenge, and possess the problem solving skills needed to decode it.
       </p>
-      <h3>Experience</h3>
-      <h5>June  2021 - September 2021</h5>
-      <p>Full Stack Student at UT Coding Bootcamp</p>
-      <h5>January  2017 - December 2020</h5>
-      <h3>PetRelocation, Austin - HR Coordinator</h3>
-      <p>Developed, facilitated and implemented all phases of the recruitment process by utilizing a RMS and a role specific rubric.</p>
-      <p>Worked with the production management department to create accurate job description posts and continuously reviewed HRIS to ensure optimal results.</p>
-      <p>Worked directly with the CEO, Cofounder,  IT management company, and HR members to  ensure all onboarding and offboarding tasks were completed and on time.</p>
-      <p>Maintained and applied knowledge of current employment laws and applicable state and federal laws and requirements as well as human resource policies and procedures to ensure compliance with law and application of best practices.</p>
-      <p>Collaborated with the marketing department to ensure website and social media content was accurate and able to attract qualified candidates.</p>
-      <p>Created, managed and maintained company wellness programs to ensure the company had a competitive edge when offering company benefits, incentives and maintaining employee satisfaction.</p>
-      <h5>February  2015 - January 2017</h5>
-      <h3>Wortham Insurance, Austin - Personal Lines Product Account Manager</h3>
-      <p>Managed 230 clients with over 520 individual policies for personal real estate, property, and more.
-      </p>
-      <p>Worked directly with existing clients to analyze their portfolio and ensure their policies are high-performing.
-      </p>
-      <p>Utilized B2B software to maintain and shop up to date products for my clients.
-      </p>
-      <p>Performed new client acquisitions matching products with client needs.</p>
+      <Nav>
+    <NavItem>
+    <NavLink style={{ textDecoration: "none", color: "#0D0D0D", fontSize: "25px", textAlign:"center", marginLeft: "500px", marginBottom:"200px"}} href="https://docs.google.com/document/d/1XK-jaZZg_fB3NbKXifW0RuK9saoeoyzQ6rxSBAJ9qBs/edit?usp=sharing">Full resume here</NavLink>
+  </NavItem>
+  </Nav>
+      
       <hr />
       <Footer></Footer>
     </div>
